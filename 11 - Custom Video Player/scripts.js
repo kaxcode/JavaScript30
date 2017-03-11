eb64eb95f5071a9c90d20ea61cd3da72dbb3f8f3
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-10 23:20:51 -0500

    Adds event listeners for click to play or pause the video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,7 +1,6 @@
 /* Get our Elements */
-const player = document.querySelector(".player");
-const video = player.querySelector(".viewer");
-
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
@@ -9,6 +8,11 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 /* Build our functions */
-
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
 
 /* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
