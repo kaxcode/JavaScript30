22030e7f77ae296f924f0c8d1ad182b129abb7f1
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-10 23:12:21 -0500

    Grab's elements needed to target them

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,10 @@
+/* Get our Elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+
+
+
+/* Build our functions */
+
+
+/* Hook up the event listeners */
