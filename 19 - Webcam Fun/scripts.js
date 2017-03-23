990e3cf1422c8acb11f7441864cd2851562b08d8
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-22 23:16:02 -0400

    Create the red filter function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,6 +24,14 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+
+    let pixels =  ctx.getImageData(0, 0, width, height);
+
+
+    // red effect
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels, 0, 0);
+
   }, 16);
 }
 
@@ -41,6 +49,15 @@ function takePhoto() {
   link.setAttribute('download', 'beautiful');
   link.innerHTML = `<img src= "${data}" alt= "Handsome Man" />`;
   strip.insertBefore(link, strip.firsChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
 }
 
 getVideo();
