7c710455922d49ea85df710f8804eed56530dd32
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-13 22:39:29 -0400

    Stops the tiemr from running when it hits 0

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,8 +1,16 @@
+let countdown;
+
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  setInterval(()=> {
+  countdown = setInterval(() => {
     const secondsLeft = (then - Date.now()) /1000;
+    //check if we should stop it!
+    if(secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    //display it
     console.log(secondsLeft);
   }, 1000);
 }
