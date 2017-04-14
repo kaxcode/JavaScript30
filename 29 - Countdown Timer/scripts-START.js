39df1604a0423ccfea19319c95b92f845a908b0f
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-13 23:03:42 -0400

    Displays the time left

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,23 +1,28 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds) {
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
 
   countdown = setInterval(() => {
-    const secondsLeft = (then - Date.now()) /1000;
-    //check if we should stop it!
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it!
     if(secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
-    //display it
+    // display it
     displayTimeLeft(secondsLeft);
   }, 1000);
 }
 
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
-  console.log(seconds);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
 }
