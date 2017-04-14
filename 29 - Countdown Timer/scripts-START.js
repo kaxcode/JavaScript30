4805a9dbc2e8b6e435e06893d2d5aecff065bb76
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-13 22:24:04 -0400

    Sets an interval to figure out how much time is left

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,5 +1,8 @@
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  console.log({now, then});
+  setInterval(()=> {
+    const secondsLeft = (then - Date.now()) /1000;
+    console.log(secondsLeft);
+  }, 1000);
 }
