bd2b9c130bb5e8d4bfc15346f664b2c4f64174de
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-15 20:02:19 -0400

    Turns timestamp into a Date

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -26,3 +26,7 @@ function displayTimeLeft(seconds) {
   document.title = display;
   timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+}
