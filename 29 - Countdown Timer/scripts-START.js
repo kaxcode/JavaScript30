f4013808f78dc19d2e97195662f143b5af2733d7
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-15 20:07:41 -0400

    Calculates at what time timer will finish

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,11 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -29,4 +31,7 @@ function displayTimeLeft(seconds) {
 
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hour}:${minutes}`;
 }
